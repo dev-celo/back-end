577bc94c99052dac4590f0de0f8e0f603f1234fc
dev-celo <marcelohenrique.backend@gmail.com>
2025-08-25 08:49:53 -0300

    fix: Correção erro de horario  formatado incorretamente corrigido.

--- src/services/cotacaoService.js
+++ src/services/cotacaoService.js
@@ -5,15 +5,21 @@ export async function buscarCotacoesCacau() {
 
   const nyQuote = await yahooFinance.quote(NY_SYMBOL);
 
-  console.log('nyQuote:', nyQuote);
-
   const marketTime = nyQuote?.regularMarketTime;
+let horarioFormatado = 'Horário indisponível';
 
-  // Garantindo que seja um número antes de multiplicar
-  const horarioFormatado =
-    typeof marketTime === 'number'
-      ? new Date(marketTime * 1000).toLocaleString('pt-BR', { timeZone: 'America/New_York' })
-      : 'Horário indisponível';
+  if (marketTime) {
+    const date = new Date(marketTime); // funciona para ISO string ou timestamp em ms
+    horarioFormatado = date.toLocaleString('pt-BR', {
+      timeZone: 'America/Bahia', // ou "America/New_York", se quiser no fuso da bolsa
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+  }
 
   return {
     bolsa: 'New York ICE',
